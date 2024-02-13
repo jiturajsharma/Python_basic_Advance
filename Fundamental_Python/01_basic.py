360088845e12ee84a9cb72090b604eda15e79b9b
JITU RAJ SHARMA <jiturajsharma8093@gmail.com>
2024-02-13 21:44:20 +0530

    class two completed

---
+++ Fundamental_Python/01_basic.py
@@ -0,0 +1,127 @@
+print("Hello World")
+
+# this is comment 
+
+def chai(n):
+    print(n)
+
+chai(50)
+chai("Testing")
+
+
+print("chai" * 20)
+
+chai = 'green'
+print(chai)
+
+
+#  core module
+
+import os
+os.getcwd()
+
+
+for c in 'chai':
+    print(c)
+
+
+    import sys
+    sys.platform
+
+
+    hello_chai = "lemon", "greentea"
+    hello_code = "black"
+
+
+
+
+username = "Jitu raj sharma"
+print(username)
+
+username = "chaiaurcode"
+print(username)
+
+
+x = 10
+y = x
+print(x)
+print(y)
+
+x = 15
+print(y)
+
+# number - > 1234, 3.256, 3+4j, 0b111, Decimal(), Fraction()
+# String :- 'spam', "Bob's", b'a/x01c, u'sp\xc4m
+# List : [1, [2, 'three'],4,5,list(range(10))]
+# Tuple : (1, 'spam', 4, 'U'), tupple('spam'), namedtuple
+# Dictonary: {'food', 'spam', 'taste': 'yum'}, dict(hours=10)
+# Set: set('abc'), {'a', 'b', 'c'}
+# File: Open(egge.txt'), open(r'C:\ham.bin', 'wb')
+#Boolen: true, false
+# None: none
+# Function , modules, classes
+# Advance: Decorators, Generators, Iterators, MetaPrograming
+
+
+myList = {1, 2, 3}
+
+
+import math
+math.pi()
+
+import random
+random.random()
+random.choice([1,2,3,4,5,6,7])
+
+name = 'Jiturajsharma'
+print(len(name))
+
+print(name[0])
+print(name[-1])
+print(name[5])
+
+print(name[1:3])
+
+
+# Disctonary
+
+myDic = {'one': 'Lemon', 'two': 'ginger'}
+print(myDic)
+
+print(myDic[0])
+print(myDic[2])
+
+print(myDic["Sharma"])
+
+import sys
+sys.getrefcount(85241)
+sys.getrefcount("Jitu raj sharma")
+
+
+
+a = 3
+a = "chaiaurcode"
+a = 3.14
+b =2
+print(a)
+print(b)
+a = a +2
+print(a)
+
+
+myListOne = [1,2,3]
+myListTwo = myListOne
+myListOne = 'chai'
+
+myListOne[0] = 33
+
+import copy
+copy(myListOne)
+
+n = [1, 2, 3]
+m = n
+print( m ==n )
+
+print(m is n)
+
+
--- python.md
+++ python.md
@@ -36,8 +36,58 @@ print("Hello World)
 * Pipe is the package manger for python you can use pip to install a module on your system
 
 
+# CHAPTER - 2 VARIABLES AND DATATYPES
+
+* A variable is the name given to a memory location in a program for example
+
+    *  primarly there are following data types in python
+    1) Integers
+    2) Floating point numbers
+    3) Strings
+    4) Booleans
+    5) None
+
+    a = 71
+    b = 88.44
+    name = "Jitu raj sharma"
+
+    * A variable name can contain alphabets, digits and underscors
+    * A variable name can only start with an alphabet and underscore
+    * A variable name can't start with a digit a variable name.
+
+    # Operators in python
+    * Arithmetic operators = + - * /
+    * Assignment operators = =+ = -= 
+    * Comparison operators = ==, > >= < , !
+    * Logical operators = and, or, not
+
+# type() function and Typecasting
+* type function is used to find the data type of a given variable in python
+
+````
+    a = 31
+    type(a) => class <int>
+    b = "31"
+    type(b)  => class <str>
+
+````
+* A number can be converted into a string and vice versa (if possible)
+* There are many functions to converted one data type into another
+
+str(31) ="31"  -> Integer to string conversion
+int('32') = 32   -> String to integer conversion
+float(32) = 32.0  -> Integer to float conversion
 
 
+# ...and so on
+*  Here "32" is a string literal and 31 a number literal
 
 
+# intput() function 
+* This function allows the user to take input from the keyboard as a string
+
+````
+a = input("Enter name") 
+````
+* It is  imported to note that the output of input is always a string(even if the number is entered)
 
